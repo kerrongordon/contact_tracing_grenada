e44d3da61a930e2bdde0038e7c1f107d51380536
kerrongordon <kgpsounds.com@gmail.com>
2021-04-24 17:33:24 -0400

    added web, linux and ci build system

--- lib/screens/screen.five.dart
+++ lib/screens/screen.five.dart
@@ -12,36 +12,38 @@ class ScreenFive extends StatelessWidget {
         title: Text('COVID-19 Survey App'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (userData.optionOne ||
-                  userData.optionTwo ||
-                  userData.optionThree)
-                Text(
-                  'There is an 85% chance you are a carrier of SARS-CoV-2, popularly known as COVID-19/The Corona Virus. Please self-quarantine for 14 days, then go to your local testing site and get tested or call the COVID-19 hotline @ (473)-438-4787 or (473) 538-4787. Thank you for using our app!',
-                  style: TextStyle(fontSize: 25),
-                  textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (userData.optionOne ||
+                    userData.optionTwo ||
+                    userData.optionThree)
+                  Text(
+                    'There is an 85% chance you are a carrier of SARS-CoV-2, popularly known as COVID-19/The Corona Virus. Please self-quarantine for 14 days, then go to your local testing site and get tested or call the COVID-19 hotline @ (473)-438-4787 or (473) 538-4787. Thank you for using our app!',
+                    style: TextStyle(fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ),
+                if (!userData.optionOne &&
+                    !userData.optionTwo &&
+                    !userData.optionThree)
+                  Text(
+                    'You are most likely catching the common cold and should rest and drink heavy fluids. However, to be certain, contact the COVID-19 hotline @ (473)-438-4787 or (473) 538-4787. Thank you for using our app!',
+                    style: TextStyle(fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ),
+                SizedBox(
+                  height: 150,
                 ),
-              if (!userData.optionOne &&
-                  !userData.optionTwo &&
-                  !userData.optionThree)
-                Text(
-                  'You are most likely catching the common cold and should rest and drink heavy fluids. However, to be certain, contact the COVID-19 hotline @ (473)-438-4787 or (473) 538-4787. Thank you for using our app!',
-                  style: TextStyle(fontSize: 25),
-                  textAlign: TextAlign.center,
+                ElevatedButton(
+                  child: Text('Done'),
+                  onPressed: () => Navigator.of(context).pop(),
                 ),
-              SizedBox(
-                height: 150,
-              ),
-              ElevatedButton(
-                child: Text('Done'),
-                onPressed: () => Navigator.of(context).pop(),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
